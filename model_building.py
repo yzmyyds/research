5496b4f61d0bfdc7402a06b32840f907750e2edc
yxds <102641394+yzmyyds@users.noreply.github.com>
2025-04-03 23:10:42 +0800

    RF+DT+SVM(part)

--- model_building.py
+++ model_building.py
@@ -14,7 +14,8 @@ corr_matrix = data.corr()
 sns.heatmap(corr_matrix[['推力/kN']].sort_values('推力/kN', ascending=False), 
             annot=True, cmap='coolwarm')
 plt.title("Pearson factors of thrust with virables")
-plt.show()
+plt.savefig("Pearson_Factors.png")
+plt.close()
 
 #############################################################################################
 #Model construction
@@ -23,26 +24,75 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 #1.Standarlize (Z-score)
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(data.drop(columns=['时间', '推力/kN']))
 X = data.drop(columns=['时间', '推力/kN'])
 y = data['推力/kN']
+#2.Split the train and test data sets
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-# 训练模型
-model = RandomForestRegressor(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+#3.Model training
 
-# 预测
-y_pred = model.predict(X_test)
-
-# 评估模型
+#3_1.Random forest
+model_RF = RandomForestRegressor(n_estimators=100, random_state=42)
+model_RF.fit(X_train, y_train)
+#3_1_1.Prediction
+y_pred = model_RF.predict(X_test)
+#3_1_2.Visualize and asess performance
 mse = mean_squared_error(y_test, y_pred)
-print(f"均方误差 (MSE): {mse}")
+score_test=model_RF.score(X_test,y_test)
+score_train=model_RF.score(X_train,y_train)
+r2=r2_score(y_test,y_pred)
+print("Random Forest:")
+print(f"MSE: {mse}")
+print(f"Accuracy of test dataset: {score_test}")
+print(f"Accuracy of train dataset: {score_train}")
+print(f"R2 score: {r2}")
 plt.figure(figsize=(10, 6))
 plt.scatter(y_test, y_pred, color='blue', alpha=0.5)
 plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')
-plt.title("实际值与预测值对比")
-plt.xlabel("实际推力/kN")
-plt.ylabel("预测推力/kN")
+plt.title("Actual vs Predict")
+plt.xlabel("Actual Thrust/kN")
+plt.ylabel("Predict Thrust/kN")
+plt.savefig("Performance_asess_of_RF.png")
+plt.close()
+
+#3_2.Decision Tree
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+model_DT=DecisionTreeRegressor(random_state=42)
+model_DT.fit(X_train,y_train)
+y_pred=model_DT.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+score_test=model_DT.score(X_test,y_test)
+score_train=model_DT.score(X_train,y_train)
+r2=r2_score(y_test,y_pred)
+print("Decision Tree:")
+print(f"MSE: {mse}")
+print(f"Accuracy of test dataset: {score_test}")
+print(f"Accuracy of train dataset: {score_train}")
+print(f"R2 score: {r2}")
+plt.figure(figsize=(10, 6))
+plt.scatter(y_test, y_pred, color='blue', alpha=0.5)
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')
+plt.title("Actual vs Predict")
+plt.xlabel("Actual Thrust/kN")
+plt.ylabel("Predict Thrust/kN")
+plt.savefig("Performance_asess_of_DT.png")
+plt.close()
+
+#3_3. SVM
+from sklearn.svm import SVR
+model_SVR=SVR(kernel='rbf', C=1.0, epsilon=0.1)
+model_SVR.fit(X_train,y_train)
+y_pred=model_SVR.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+score_test=model_SVR.score(X_test,y_test)
+score_train=model_SVR.score(X_train,y_train)
+r2=r2_score(y_test,y_pred)
+print("SVM:")
+print(f"MSE: {mse}")
+print(f"Accuracy of test dataset: {score_test}")
+print(f"Accuracy of train dataset: {score_train}")
+print(f"R2 score: {r2}")
 plt.show()
